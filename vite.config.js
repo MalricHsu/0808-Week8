d778d2328e63282a13e7f80f180047889af5bc5e
Malric <qinglun089@google.com>
2025-08-16 23:30:19 +0800

    2 commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: "/0808-WEEK8/",
+  base: "/0808-Week8/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
